429d8f9cf65dd7f341d01f477c02ec31be785ad6
m <mmailm@mail.ru>
2021-02-03 00:19:22 +0800

    fix

--- src/db.ts
+++ src/db.ts
@@ -3,37 +3,16 @@ import pg = require('pg');
 import {remote } from "electron";
 import * as fs from "fs";
 import path from "path";
-let app = remote.app
+import ConfigStore from "./store/config_store";
 
-let userPath = app.getPath('userData')
 
-if (!fs.existsSync(userPath))
-    fs.mkdirSync(userPath)
-
-export let options: any = {
-    username: "postgres",
-    password: 123,
-    database: "tealeaf2019",
-    port: 5432,
-    host: "localhost",
-    dialect: "postgres",
-}
-
-try {
-    let data = fs.readFileSync(path.join(userPath, "config.json"))
-    options = JSON.parse(data.toString())
-} catch(e) {
-    fs.writeFileSync(path.join(userPath, "config.json"), JSON.stringify(options))
-    console.error(e)
-}
 
 export const db = new Sequelize({
-   ...options,
+   ...ConfigStore.instance.sequelize,
     dialectModule: pg,
 });
 
 
 module.exports = {
     db: db,
-    options: options,
 };
---
+++ src/store/config_store.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import {remote} from "electron";
+import path from "path";
+import _ from 'lodash'
+import {Options} from "sequelize";
+
+let app = remote.app
+
+interface ConfigStoreOptions {
+    sequelize: Options
+}
+
+
+export default class ConfigStore {
+    private static _instance: ConfigStore = new ConfigStore();
+
+    private _userPath = app.getPath('userData')
+    private _configPath = path.join(this._userPath, "config.json")
+    private _options: ConfigStoreOptions;
+
+    private defaultOptions: ConfigStoreOptions = {
+        sequelize: {
+            username: "postgres",
+            password: "123",
+            database: "tealeaf2019",
+            port: 5432,
+            host: "localhost",
+            dialect: "postgres",
+        }
+    }
+
+    constructor() {
+        if (ConfigStore._instance) {
+            throw new Error("Error: Instantiation failed: Use ConfigStore.getInstance() instead of new.");
+        }
+        ConfigStore._instance = this;
+    }
+
+    static get instance() {
+        return ConfigStore._instance
+    }
+
+    get options() {
+        if (!fs.existsSync(this._userPath))
+            fs.mkdirSync(this._userPath)
+
+        try {
+            let data = fs.readFileSync(this._configPath)
+            this._options = JSON.parse(data.toString())
+        } catch (e) {
+            this._options = _.cloneDeep(this.defaultOptions)
+        }
+        return this._options
+    }
+
+    set options(value) {
+        this._options = value;
+        fs.writeFileSync(this._configPath, JSON.stringify(this._options))
+    }
+
+    get sequelize(): Options {
+        return _.get(this.options, "sequelize", this.defaultOptions.sequelize)
+    }
+
+    set sequelize(value: Options) {
+        let sequelize = {...this.sequelize, ...value}
+        this.options = {
+            ...this.options,
+            sequelize: sequelize
+        }
+    }
+}
